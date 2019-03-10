0ddf8032ebea74078417040c81025d7d1c8cddaa
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-03-10 17:24:59 -0500

    Added modal to enter a new task

--- app/src/main/java/com/example/yourtaskapp/HomeActivity.java
+++ app/src/main/java/com/example/yourtaskapp/HomeActivity.java
@@ -2,8 +2,10 @@ package com.example.yourtaskapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -25,7 +27,17 @@ public class HomeActivity extends AppCompatActivity {
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder myDialog = new AlertDialog.Builder(HomeActivity.this);
 
+                LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+
+                View myView = inflater.inflate(R.layout.custom_input_field, null);
+
+                myDialog.setView(myView);
+
+                AlertDialog dialog = myDialog.create();
+
+                dialog.show();
             }
         });
     }
