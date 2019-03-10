a607de5c6744b7e5187193ba153598b097334d6e
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-03-10 17:32:02 -0500

    Added get data of View

--- app/src/main/java/com/example/yourtaskapp/HomeActivity.java
+++ app/src/main/java/com/example/yourtaskapp/HomeActivity.java
@@ -5,8 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -36,6 +39,28 @@ public class HomeActivity extends AppCompatActivity {
                 myDialog.setView(myView);
 
                 AlertDialog dialog = myDialog.create();
+
+                final EditText title = myView.findViewById(R.id.edt_title);
+                final EditText note = myView.findViewById(R.id.edt_note);
+                Button btnSave = myView.findViewById(R.id.btn_save);
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        if (TextUtils.isEmpty(mTitle)) {
+                            title.setError("Required field...");
+                            return;
+                        }
+
+                        if (TextUtils.isEmpty(mNote)) {
+                            note.setError("Required field...");
+                            return;
+                        }
+                    }
+                });
 
                 dialog.show();
             }
